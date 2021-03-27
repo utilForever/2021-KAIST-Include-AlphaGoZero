e5f7d504296448dd0df57cd3d2ce6e4bc5c55ee3
Chris Ohk <utilforever@gmail.com>
2021-03-27 19:45:19 +0900

    Upload code for 'Implementing first Go bot'

---
+++ 2 - Code/210327 - Implementing first Go bot/dlgo/gotypes.py
@@ -0,0 +1,21 @@
+import enum
+from collections import namedtuple
+
+
+class Player(enum.Enum):
+    black = 1
+    white = 2
+
+    @property
+    def other(self):
+        return Player.black if self == Player.white else Player.white
+
+
+class Point(namedtuple('Point', 'row col')):
+    def neighbors(self):
+        return [
+            Point(self.row - 1, self.col),
+            Point(self.row + 1, self.col),
+            Point(self.row, self.col - 1),
+            Point(self.row, self.col + 1),
+        ]
---
+++ 2 - Code/210327 - Implementing first Go bot/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='dlgo',
+      version='0.1',
+      description='Deep Learning and the Game of Go',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
