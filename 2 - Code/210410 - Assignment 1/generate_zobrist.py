e0e35bdb65d494b3d5397f8427f362bb6661d65c
Chris Ohk <utilforever@gmail.com>
2021-04-10 14:16:48 +0900

    Upload code for 'Assignment 1'

---
+++ 2 - Code/210410 - Assignment 1/generate_zobrist.py
@@ -0,0 +1,33 @@
+import random
+
+from dlgo.gotypes import Player, Point
+
+
+def to_python(player_state):
+    if player_state is None:
+        return 'None'
+    if player_state == Player.black:
+        return Player.black
+    return Player.white
+
+
+MAX63 = 0x7fffffffffffffff
+
+table = {}
+empty_board = 0
+for row in range(1, 20):
+    for col in range(1, 20):
+        for state in (Player.black, Player.white):
+            code = random.randint(0, MAX63)
+            table[Point(row, col), state] = code
+
+print('from .gotypes import Player, Point')
+print('')
+print("__all__ = ['HASH_CODE', 'EMPTY_BOARD']")
+print('')
+print('HASH_CODE = {')
+for (pt, state), hash_code in table.items():
+    print('    (%r, %s): %r,' % (pt, to_python(state), hash_code))
+print('}')
+print('')
+print('EMPTY_BOARD = %d' % (empty_board,))
