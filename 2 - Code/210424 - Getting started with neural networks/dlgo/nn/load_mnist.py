e59e2a72c0ced0d708b2c3607a5af7e8b01dc6ee
Chris Ohk <utilforever@gmail.com>
2021-05-11 23:57:26 +0900

    Change code to load MNIST data

--- 2 - Code/210424 - Getting started with neural networks/dlgo/nn/load_mnist.py
+++ 2 - Code/210424 - Getting started with neural networks/dlgo/nn/load_mnist.py
@@ -11,25 +11,13 @@ def encode_label(j):
 
 def shape_data(data):
     features = [np.reshape(x, (784, 1)) for x in data[0]]
-
     labels = [encode_label(y) for y in data[1]]
-
     return list(zip(features, labels))
 
 
-def load_data_impl():
-    # file retrieved by:
-    #   wget https://s3.amazonaws.com/img-datasets/mnist.npz -O code/dlgo/nn/mnist.npz
-    # code based on:
-    #   site-packages/keras/datasets/mnist.py
-    path = 'mnist.npz'
-    f = np.load(path)
-    x_train, y_train = f['x_train'], f['y_train']
-    x_test, y_test = f['x_test'], f['y_test']
-    f.close()
-    return (x_train, y_train), (x_test, y_test)
-
-
 def load_data():
-    train_data, test_data = load_data_impl()
+    with gzip.open('mnist.pkl.gz', 'rb') as f:
+        data = pickle._Unpickler(f)
+        data.encoding = 'latin1'
+        train_data, validation_data, test_data = data.load()
     return shape_data(train_data), shape_data(test_data)
